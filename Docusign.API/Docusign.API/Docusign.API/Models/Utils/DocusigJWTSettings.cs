8766ee35df283a59ee77e20b78a78f443b7efecd
liliya.galina@neadevis.com <liliya.galina@neadevis.com>
2021-06-05 11:51:02 +0300

    Add settings classes

---
+++ Docusign.API/Docusign.API/Docusign.API/Models/Utils/DocusigJWTSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Docusign.API.Models.Utils
+{
+    public class DocusigJWTSettings
+    {
+        public string ClientId { get; set; }
+        public string ImpersonatedUserId { get; set; }
+        public string AuthServer { get; set; }
+        public string PrivateKeyFile { get; set; }
+    }
+}
---
+++ Docusign.API/Docusign.API/Docusign.API/Models/Utils/DocusignSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Docusign.API.Models.Utils
+{
+    public class DocusignSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string AuthorizationEndpoint { get; set; }
+        public string TokenEndpoint { get; set; }
+        public string UserInformationEndpoint { get; set; }
+        public string AppUrl { get; set; }
+        public string RoomsApiEndpoint { get; set; }
+        public string SignerEmail { get; set; }
+        public string SignerName { get; set; }
+        public string GatewayAccountId { get; set; }
+        public string GatewayName { get; set; }
+        public string GatewayDisplayName { get; set; }
+    }
+}
